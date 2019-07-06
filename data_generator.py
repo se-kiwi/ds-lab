9f4264cdcffa68592e135875b4abc482610971d9
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2019-07-06 21:11:05 +0800

    modify generator

--- data_generator.py
+++ data_generator.py
@@ -1,11 +1,11 @@
-from random import choice, seed, randint
+import random
+from random import choice, randint
 import json, dataclasses,time
 from dataclasses import dataclass
 
 concurrency = ["CNY", "USD", "JPY", "EUR"]
 commodity = ["1", "2", "3", "4"]
 users = [1, 2, 3, 4, 5, 6, 7]
-seed()
 
 MAX_ITEM = 5
 MAX_PURCHASE = 5
@@ -17,8 +17,11 @@ class Item:
     number: int
 
 class ItemFactory:
+    @staticmethod
     def create_random():
         return Item(choice(commodity), randint(1, MAX_ITEM))
+
+    @staticmethod
     def create_random_list(num):
         return [ItemFactory.create_random() for i in range(num)]
 
@@ -32,9 +35,12 @@ class Record:
 nowtime = lambda : int(round(time.time() * 1000))
 
 class RecordFactory:
+    @staticmethod
     def create_random():
         return Record(choice(users), choice(concurrency),\
             nowtime(), ItemFactory.create_random_list(randint(1, MAX_PURCHASE)))
+    
+    @staticmethod
     def create_random_list(num):
         return [RecordFactory.create_random() for i in range(num)]
 
@@ -45,10 +51,16 @@ class Generator:
                 return dataclasses.asdict(o)
             return super().default(o)
 
+    @staticmethod
     def generate(num):
         t = RecordFactory.create_random_list(num)
-        return json.dumps(t, cls=Generator.EnhancedJSONEncoder)
+        ans = ""
+        for i in t:
+            ans += json.dumps(i, cls=Generator.EnhancedJSONEncoder)
+            ans += "\n"
+        return ans
 
+    @staticmethod
     def generate_to_file(num, file):
         offset = num % GENERATE_STRIDE
         num = num - offset
@@ -61,6 +73,7 @@ class Generator:
         
 
 if __name__ == "__main__":
-    Generator.generate_to_file(20, "test-1.json")
+    random.seed()
+    Generator.generate_to_file(20, "test-1.data")
     
         
