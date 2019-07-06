b91a5351642f76fdade7d19a2025f15ed0991fd3
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2019-07-06 22:56:51 +0800

    add http sender

---
+++ src/main/java/com/kiwi/dslab/Config.java
@@ -0,0 +1,5 @@
+package com.kiwi.dslab;
+
+public final class Config {
+    public static final int PORT = 8080;
+}
--- src/main/java/com/kiwi/dslab/KafkaProperties.java
+++ src/main/java/com/kiwi/dslab/KafkaProperties.java
@@ -1,6 +1,6 @@
 package com.kiwi.dslab;
 
-public class KafkaProperties {
+public final class KafkaProperties {
     public static final String ZK="localhost:2181";      //Zookeeper地址
     public static final String TOPIC="yfzm";                          //topic名称
     public static final String BROKER_LIST="localhost:9092";    //Broker列表
