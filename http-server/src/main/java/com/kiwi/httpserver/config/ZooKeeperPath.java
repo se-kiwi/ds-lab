e47ed3cd7ad4363ebc9ebc74da2517d2e2b6e3c8
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2019-07-07 20:42:57 +0800

    add api to get transaction amount

---
+++ http-server/src/main/java/com/kiwi/httpserver/config/ZooKeeperPath.java
@@ -0,0 +1,5 @@
+package com.kiwi.httpserver.config;
+
+public final class ZooKeeperPath {
+    public static final String ZK_PATH_TXAMOUNT = "/kiwi/txAmount";
+}
---
+++ http-server/src/main/java/com/kiwi/httpserver/zookeeper/ZkDaoImpl.java
@@ -0,0 +1,58 @@
+package com.kiwi.httpserver.zookeeper;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+import static com.kiwi.httpserver.config.ZooKeeperPath.ZK_PATH_TXAMOUNT;
+
+public class ZkDaoImpl implements ZkDao {
+
+    private ZooKeeper zk = null;
+
+    public ZkDaoImpl() {
+        try {
+            final CountDownLatch connectedSignal = new CountDownLatch(1);
+            zk = new ZooKeeper("localhost:2181", 5000, event -> {
+                if (event.getState() == Watcher.Event.KeeperState.SyncConnected) {
+                    connectedSignal.countDown();
+                }
+            });
+            connectedSignal.await();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            zk = null;
+        }
+    }
+
+    @Override
+    public Double getTotalTransactionAmount() {
+        return getDoubleByPath(ZK_PATH_TXAMOUNT);
+    }
+
+    private double getDoubleByPath(String path) {
+        if (zk == null) return -1;
+        Stat stat = new Stat();
+        try {
+            return Double.valueOf(new String(zk.getData(path, false, stat)));
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    @Override
+    public void close() {
+        if (zk != null) {
+            try {
+                zk.close();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
