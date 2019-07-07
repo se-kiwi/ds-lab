b0bf4db89bcd6b6d39f7711396551ae5d4401b40
WangPuyue <wangpy1489@sjtu.edu.cn>
2019-07-07 18:16:51 +0800

    update exchange every 1min

--- dslab/src/main/java/com/kiwi/dslab/ExchangeSchedule.java
+++ dslab/src/main/java/com/kiwi/dslab/ExchangeSchedule.java
@@ -9,23 +9,32 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class ExchangeSchedule {
-    private static ZkDaoImpl zDao;
+//    private static ZkDaoImpl zDao = new ZkDaoImpl();
 
     private static Runnable RMBRunner = () -> {
         System.out.println("RMB round:" + new Date());
+        ZkDaoImpl zDao = new ZkDaoImpl();
         zDao.changeExchangeRateRandomly("RMB");
+        zDao.close();
     };
     private static Runnable USDRunner = () -> {
         System.out.println("USD round:" + new Date());
+        ZkDaoImpl zDao = new ZkDaoImpl();
         zDao.changeExchangeRateRandomly("USD");
+        zDao.close();
+
     };
     private static Runnable JPYRunner = () -> {
         System.out.println("JPY round:" + new Date());
+        ZkDaoImpl zDao = new ZkDaoImpl();
         zDao.changeExchangeRateRandomly("JYP");
+        zDao.close();
     };
     private static Runnable EURRunner = () -> {
         System.out.println("EUR round:" + new Date());
+        ZkDaoImpl zDao = new ZkDaoImpl();
         zDao.changeExchangeRateRandomly("EUR");
+        zDao.close();
     };
 
 
