45d7677c201789a172e025e32e97184ae6bd2e33
Li Hao <yfzm1223@sjtu.edu.cn>
2019-07-08 23:25:16 +0800

    finish distributed lock test

--- dslab/src/main/java/com/kiwi/dslab/zookeeper/LockTest.java
+++ dslab/src/main/java/com/kiwi/dslab/zookeeper/LockTest.java
@@ -1,15 +1,21 @@
 package com.kiwi.dslab.zookeeper;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CountDownLatch;
 
+import static com.kiwi.dslab.KafkaProperties.ZK;
+
 public class LockTest {
+
     public static void main(String[] args) throws Exception {
-        String host = args[0];
-        String path = args[1];
-        String myName = args[2];
+        String host = ZK;
+        String path = "/lock";
         final CountDownLatch connectedSignal = new CountDownLatch(1);
         ZooKeeper zk = new ZooKeeper(host, 5000, event -> {
             if (event.getState() == Watcher.Event.KeeperState.SyncConnected) {
@@ -18,15 +24,42 @@ public class LockTest {
         });
         connectedSignal.await();
 
-        DistributedLock lock = new DistributedLock(myName, zk, path);
+        new Thread(() -> {
+            DistributedLock lock = new DistributedLock("lock1", zk, "/lock");
+            try {
+                lock.lock();
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("111111111111");
+                    Thread.sleep(100);
+                }
+                lock.unlock();
+            } catch (KeeperException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            DistributedLock lock = new DistributedLock("lock2", zk, "/lock");
+            try {
+                lock.lock();
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("222222222222");
+                    Thread.sleep(100);
+                }
+                lock.unlock();
+            } catch (KeeperException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
-        lock.lock();
-        xixixi();
-        lock.unlock();
+//        DistributedLock lock = new DistributedLock("test", zk, path);
+//
+//        lock.lock();
+//        xixixi();
+//        lock.unlock();
 
     }
 
     private static void xixixi() {
-
+        System.out.println("xixixi");
     }
 }
