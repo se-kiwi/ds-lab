8f7eecc7be331ccfa7bbdd2fa1a0b744b17354bc
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2019-07-11 21:01:16 +0800

    update config

--- http-server/src/main/java/com/kiwi/httpserver/config/KafkaProperties.java
+++ http-server/src/main/java/com/kiwi/httpserver/config/KafkaProperties.java
@@ -1,8 +1,6 @@
 package com.kiwi.httpserver.config;
 
 public final class KafkaProperties {
-    public static final String ZK="localhost:2181";      //Zookeeper地址
     public static final String TOPIC="order";                   //topic名称
-    public static final String BROKER_LIST="localhost:9092";    //Broker列表
-    public static final String GROUP_ID="group01";              //消费者使用
+    public static final String BROKER_LIST="10.233.36.48:9092";    //Broker列表
 }
--- http-server/src/main/java/com/kiwi/httpserver/config/ZooKeeperConfig.java
+++ http-server/src/main/java/com/kiwi/httpserver/config/ZooKeeperConfig.java
@@ -2,6 +2,6 @@ package com.kiwi.httpserver.config;
 
 public final class ZooKeeperConfig {
     public static final String ZK_PATH_TXAMOUNT = "/kiwi/txAmount";
-    public static final String ZK_HOST_PORT = "localhost:2181";
+    public static final String ZK_HOST_PORT = "10.233.19.135:2181";
     public static final int ZK_TIMEOUT = 5000;
 }
